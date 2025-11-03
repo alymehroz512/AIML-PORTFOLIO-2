e625d8f7c64e4dbff3b7e1756d67090c8c37b451
Ali Mehroz <alimehroz621@gmail.com>
2025-11-03 19:46:32 +0500

    Navbar slice is also updated

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/CustomNavbar.css';
-import Logo from '../assets/Logo.svg';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { collapseNav, toggleExpanded } from "../redux/navSlice";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/CustomNavbar.css";
+import Logo from "../assets/Logo.svg";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 
 const CustomNavbar = () => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleNavClick = () => setExpanded(false);
+  const dispatch = useDispatch();
+  const { isExpanded, links } = useSelector((state) => state.nav);
 
   return (
     <Navbar
       expand="lg"
       className="bg-black"
       fixed="top"
-      expanded={expanded}
-      onToggle={(nextExpanded) => setExpanded(nextExpanded)}
+      expanded={isExpanded}
+      onToggle={() => dispatch(toggleExpanded())}
     >
       {/* Logo */}
       <Navbar.Brand>
@@ -26,33 +27,23 @@ const CustomNavbar = () => {
 
       {/* Mobile Toggle Icon */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-0">
-        {expanded ? <HiX color="white" size={24} /> : <HiMenuAlt3 color="white" size={24} />}
+        {isExpanded ? <HiX color="white" size={24} /> : <HiMenuAlt3 color="white" size={24} />}
       </Navbar.Toggle>
 
       {/* Navbar Links */}
       <Navbar.Collapse id="basic-navbar-nav" className="ms-3">
         <Nav className="me-auto mt-1 mx-0">
-          <Nav.Link as={NavLink} to="/" onClick={handleNavClick} className="text-white nav-link-custom">
-            Home
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/about" onClick={handleNavClick} className="text-white nav-link-custom">
-            About
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/skills" onClick={handleNavClick} className="text-white nav-link-custom">
-            Expertise
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/experience" onClick={handleNavClick} className="text-white nav-link-custom">
-            Experinece
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/projects" onClick={handleNavClick} className="text-white nav-link-custom">
-            Projects
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/devtools" onClick={handleNavClick} className="text-white nav-link-custom">
-            Toolkit
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/contact" onClick={handleNavClick} className="text-white nav-link-custom">
-            Contact
-          </Nav.Link>
+          {links.map((link, index) => (
+            <Nav.Link
+              as={NavLink}
+              to={link.path}
+              key={index}
+              onClick={() => dispatch(collapseNav())}
+              className="text-white nav-link-custom"
+            >
+              {link.label}
+            </Nav.Link>
+          ))}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/redux/navSlice.js
+++ src/redux/navSlice.js
@@ -1,16 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+// src/redux/navSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isExpanded: false,
+  links: [
+    { label: "Home", path: "/" },
+    { label: "About", path: "/about" },
+    { label: "Expertise", path: "/skills" },
+    { label: "Experience", path: "/experience" },
+    { label: "Projects", path: "/projects" },
+    { label: "Toolkit", path: "/devtools" },
+    { label: "Contact", path: "/contact" },
+  ],
+};
 
 const navSlice = createSlice({
-  name: 'nav',
-  initialState: {
-    isExpanded: false,
-  },
+  name: "nav",
+  initialState,
   reducers: {
+    // Set the expanded state directly (used by Navbar)
     setExpanded: (state, action) => {
       state.isExpanded = action.payload;
+    },
+
+    // Toggle navbar expanded/collapsed state
+    toggleExpanded: (state) => {
+      state.isExpanded = !state.isExpanded;
+    },
+
+    // Collapse navbar when a link is clicked
+    collapseNav: (state) => {
+      state.isExpanded = false;
+    },
+
+    // Dynamically update navigation links (optional)
+    setNavLinks: (state, action) => {
+      state.links = action.payload;
     },
   },
 });
 
-export const { setExpanded } = navSlice.actions;
+export const { setExpanded, toggleExpanded, collapseNav, setNavLinks } =
+  navSlice.actions;
+
 export default navSlice.reducer;
